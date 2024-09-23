c2de9dc665c8e202d29a91b7a514ebd7bb5eceb4
Paralleltree <paralleltree@outlook.com>
2024-09-23 14:08:41 +0900

    Add NDMF plugin definition

---
+++ Editor/AvatarAidNdmfPlugin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nadena.dev.ndmf;
+
+[assembly: ExportsPlugin(typeof(Paltee.AvatarAid.AvatarAidNdmfPlugin))]
+
+namespace Paltee.AvatarAid
+{
+    public class AvatarAidNdmfPlugin : Plugin<AvatarAidNdmfPlugin>
+    {
+        public override string QualifiedName => "dev.paltee.avatar-aid";
+        public override string DisplayName => "AvatarAid";
+
+        protected override void Configure()
+        {
+            InPhase(BuildPhase.Generating).Run("Apply FaceEmote", ctx => new FaceEmoteProcessor().Process(ctx));
+        }
+    }
+}
